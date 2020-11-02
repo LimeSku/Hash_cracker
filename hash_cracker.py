0eb925603462240c9a0d0b9ec19e11454420bc3b
Lime <64971922+Lime667@users.noreply.github.com>
2020-11-02 20:53:47 +0100

    update, optimization, default wordlist added

--- hash_cracker.py
+++ hash_cracker.py
@@ -1,4 +1,5 @@
-import hashlib, os, sys, time, threading
+import hashlib, os, sys, time
+from threading import Thread
 from colorama import Fore, Back, Style, init
 init()
 usage = Fore.YELLOW + """
@@ -6,75 +7,87 @@ usage = Fore.YELLOW + """
   Arg1 -- The hash's algorithm           |          MD5 ----------- md5
   Arg2 -- The hash you wants to crack    |       SHA256 ----------- sha256
   Arg3 -- The path to the wordlist       |       SHA512 ----------- sha512
-
+  
   Example : python3 <hash_type> <your_hash> <path_to_the_wordlist>
+  Example : python3 <hash_type> <your_hash> #select the default wordlist (rockyou)
+  "sUtOr, nE SuPrA CrEpIdAm"...
 """ + Fore.RESET
 
-def md5_decrypt(UA):
-    t1 = time.time()
-    counter = 0
-    print("[*] Attacking md5 hash",UA, " by bruteforce.")
-    for line in file:
-        line = line.replace("\n","")
-        md5_hash = hashlib.md5(line.encode()).hexdigest()
-        counter += 1
-        if md5_hash == UA:
-            t2 = time.time()
-            tt = t2 - t1
-            try :
-                HR = counter/tt
-            except:
-                HR = "Too short interval to properly mesuring hashrate"
+class bruteforcing:
+    def md5_decrypt(UA):
+        t1 = time.time()
+        global counter
+        print("[*] Attacking md5 hash",UA, "by bruteforce.")
+        for line in file:
+            line = line.replace("\n","")
+            guess = hashlib.md5(line.encode()).hexdigest()
+            counter += 1
+            bruteforcing.final(UA,line,counter,guess,t1)
+
+    def sha256_decrypt(UA):
+        t1 = time.time()
+        global counter
+        print("[*] Attacking Sha256 hash",UA,"by bruteforce.")
+        for line in file:
+            line = line.replace("\n","")
+            guess = hashlib.sha256(line.encode()).hexdigest()
+            counter += 1
+            bruteforcing.final(UA,line,counter,guess,t1)
+
+    def sha512_decrypt(UA):
+        t1 = time.time()
+        global counter
+        print("[*] Attacking sha512 hash", UA, "by bruteforce.")
+        for line in file:
+            line = line.replace("\n","")
+            guess = hashlib.sha512(line.encode()).hexdigest()
+            counter += 1
+            bruteforcing.final(UA,line,counter,guess,t1)
+
+    def final(UA,line,counter,guess,t1):
+        if guess == UA:
+            HR,tt = utilitaries.hashratebf(t1)
             print("[*] The corresponding string to",UA,"is "+Fore.RED+line+Fore.RESET)
             print("[*] Found in "+Fore.RED,tt,"seconds",Fore.RESET,"and",Fore.RED,counter,"hash tested",Fore.RESET)
             print("[+] HashRate in",sys.argv[1],":"+Fore.YELLOW,HR,"/s"+Fore.RESET)
-            break
+            exit()
 
-def sha256_decrypt(UA):
-    t1 = time.time()
-    counter = 0
-    print("[*] Attacking Sha256 hash",UA,"by bruteforce.")
-    for line in file:
-        line = line.replace("\n","")
-        sha256_hash = hashlib.sha256(line.encode()).hexdigest()
-        counter += 1
-        if sha256_hash == UA:
-            t2 = time.time()
-            tt = t2 - t1
+class utilitaries:
+    def define_mode():
+        if sys.argv[1] == "md5" or sys.argv[1] == "MD5":
+            bruteforcing.md5_decrypt(UserHash)
+        if sys.argv[1] == "sha256" or sys.argv[1] == "SHA256":
+            bruteforcing.sha256_decrypt(UserHash)
+        if sys.argv[1] == "sha512" or sys.argv[1] == "SHA512":
+            bruteforcing.sha512_decrypt(UserHash)
+
+    def clear():
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def hashratebf(t1):
+        t2 = time.time()
+        tt = t2 - t1
+        try :
             HR = counter/tt
-            print("[*] The corresponding string to",UA,"is "+Fore.RED+line+Fore.RESET)
-            print("[*] Found in "+Fore.RED,tt,"seconds",Fore.RESET,"and",Fore.RED,counter,"hash tested",Fore.RESET)
-            print("[+] HashRate in",sys.argv[1],":"+Fore.YELLOW,HR,"/s"+Fore.RESET)
-            break
+        except:
+            HR = "Too short interval to properly mesuring hashrate"
+        return HR,tt
 
-def sha512_decrypt(UA):
-    t1 = time.time()
-    counter = 0
-    print("[*] Attacking sha512 hash", UA, "by bruteforce.")
-    for line in file:
-        line = line.replace("\n","")
-        sha512_hash = hashlib.sha512(line.encode()).hexdigest()
-        counter += 1
-        if sha512_hash == UA:
-            t2 = time.time()
-            tt = t2 - t1
-            HR = counter/tt
-            print("[*] The corresponding string to",UA,"is "+Fore.RED+line+Fore.RESET)
-            print("[*] Found in "+Fore.RED,tt,"seconds",Fore.RESET,"and",Fore.RED,counter,"hash tested",Fore.RESET)
-            print("[+] HashRate in",sys.argv[1],":"+Fore.YELLOW,HR,"/s"+Fore.RESET)
-            break
 
-if len(sys.argv) < 4:
-    os.system('cls' if os.name == 'nt' else 'clear')
+counter = 0
+if len(sys.argv) < 3:
+    utilitaries.clear()
     print(usage)
+elif len(sys.argv) == 3:
+    utilitaries.clear()
+    UserHash = sys.argv[2]
+    print("selecting default wordlist (rockyou)...")
+    path_to_WL = "rockyou.txt"
+    file = open(path_to_WL,"r",errors='ignore')
+    utilitaries.define_mode()
 else:
-    os.system('cls' if os.name == 'nt' else 'clear')
+    utilitaries.clear()
     UserHash = sys.argv[2]
     path_to_WL = sys.argv[3]
     file = open(path_to_WL,"r",errors='ignore')
-    if sys.argv[1] == "md5" or sys.argv[1] == "MD5":
-        md5_decrypt(UserHash)
-    if sys.argv[1] == "sha256" or sys.argv[1] == "SHA256":
-        sha256_decrypt(UserHash)
-    if sys.argv[1] == "sha512" or sys.argv[1] == "SHA512":
-        sha512_decrypt(UserHash)
+    utilitaries.define_mode()
