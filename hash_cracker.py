4949100c8f852e5bf3ec3916ed2c50eea8f3f84a
Lime <64971922+Lime667@users.noreply.github.com>
2020-11-07 20:35:03 +0100

    update, nt, lm

--- hash_cracker.py
+++ hash_cracker.py
@@ -1,20 +1,22 @@
 import hashlib, os, sys, time
-from threading import Thread
 from colorama import Fore, Back, Style, init
 from tqdm import tqdm
+from passlib.handlers.windows import lmhash, nthash
 init()
 usage = Fore.YELLOW + """
               Usage :                    |            Available HASH : 
   Arg1 -- The hash's algorithm           |          MD5 ----------- md5
   Arg2 -- The hash you wants to crack    |       SHA256 ----------- sha256
   Arg3 -- The path to the wordlist       |       SHA512 ----------- sha512
-
+                                                   NT --------------- Nt
+                                                   LM --------------- Lm
+                                                   
   Example : python3 <hash_type> <your_hash> <path_to_the_wordlist>
   Example : python3 <hash_type> <your_hash> #select the default wordlist (rockyou) 
 """ + Fore.RESET
 
 class bruteforcing:
-    def md5_decrypt(UA):
+    def md5(UA):
         t1 = time.time()
         global counter
         print("[*] Attacking md5 hash",UA, "by bruteforce.")
@@ -24,7 +26,7 @@ class bruteforcing:
             counter += 1
             bruteforcing.final(UA,line,counter,guess,t1)
 
-    def sha256_decrypt(UA):
+    def sha256(UA):
         t1 = time.time()
         global counter
         print("[*] Attacking Sha256 hash",UA,"by bruteforce.")
@@ -34,7 +36,7 @@ class bruteforcing:
             counter += 1
             bruteforcing.final(UA,line,counter,guess,t1)
 
-    def sha512_decrypt(UA):
+    def sha512(UA):
         t1 = time.time()
         global counter
         print("[*] Attacking sha512 hash", UA, "by bruteforce.")
@@ -47,20 +49,47 @@ class bruteforcing:
     def final(UA,line,counter,guess,t1):
         if guess == UA:
             HR,tt = utilitaries.hashratebf(t1)
-            print("[*] The corresponding string to",UA,"is "+Fore.RED+line+Fore.RESET)
+            print("\n[*] The corresponding string to",UA,"is "+Fore.RED+line+Fore.RESET)
             print("[*] Found in "+Fore.RED,tt,"seconds",Fore.RESET,"and",Fore.RED,counter,"hash tested",Fore.RESET)
             print("[+] HashRate in",sys.argv[1],":"+Fore.YELLOW,HR,"/s"+Fore.RESET)
             exit()
 
+
+class windows:
+    def ntH(UA):
+        t1 = time.time()
+        global counter
+        print("[*] Attacking NT hash : {}".format(UA))
+        for line in tqdm(file,total=len(list(open(path_to_WL,"rb"))),unit="pass"):
+            line = line.replace("\n","")
+            guess = nthash.hash(line.encode())
+            counter += 1
+            bruteforcing.final(UA,line,counter,guess,t1)
+
+    def lmH(UA):
+        t1 = time.time()
+        global counter
+        print("[*] Attacking LM hash : {}".format(UA))
+        for line in tqdm(file,total=len(list(open(path_to_WL,"rb"))),unit="pass"):
+            line = line.replace("\n","")
+            guess = lmhash.hash(line.encode())
+            counter += 1
+            bruteforcing.final(UA,line,counter,guess,t1)
+
+
+
 class utilitaries:
     def define_mode():
         if sys.argv[1] == "md5" or sys.argv[1] == "MD5":
-            bruteforcing.md5_decrypt(UserHash)
+            bruteforcing.md5(UserHash)
         if sys.argv[1] == "sha256" or sys.argv[1] == "SHA256":
-            bruteforcing.sha256_decrypt(UserHash)
+            bruteforcing.sha256(UserHash)
         if sys.argv[1] == "sha512" or sys.argv[1] == "SHA512":
-            bruteforcing.sha512_decrypt(UserHash)
-
+            bruteforcing.sha512(UserHash)
+        if sys.argv[1] == "nt" or sys.argv[1] == "NT" or sys.argv[1] == "Nt":
+            windows.ntH(UserHash)
+        if sys.argv[1] == "lm" or sys.argv[1] == "LM" or sys.argv[1] == "Lm":
+            windows.lmH(UserHash)
     def clear():
         os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -72,7 +101,6 @@ class utilitaries:
         except:
             HR = "Too short interval to properly mesuring hashrate"
         return HR,tt
-
 
 counter = 0
 if len(sys.argv) < 3:
